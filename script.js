ccf80e51095c6359db6d9fbd9d89fe7f4478cff3
Jordanjhiggins <jordan.higgins14@gmail.com>
2022-12-11 12:04:27 -0400

    add function to display newBook

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const closeModalButtons = document.querySelectorAll("[data-close-button]");
 const overlay = document.getElementById("overlay");
 // Card elements
 const cardContainer = document.getElementById("card-container");
-const cardTitle = document.querySelector(".card-title");
-const cardAuthor = document.querySelector(".card-author");
-const cardPages = document.querySelector(".card-pages");
+// const cardTitle = document.querySelector(".card-title");
+// const cardAuthor = document.querySelector(".card-author");
+// const cardPages = document.querySelector(".card-pages");
 // clone of card
-const clone = document.querySelector(".card").cloneNode(true);
+// const clone = document.querySelector(".card").cloneNode(true);
 
 // Main App
 let myLibrary = [];
@@ -20,13 +20,31 @@ function Book(title, author, pages) {
 }
 
 function addBookToLibrary() {
-  const newBook = Object.create(Book);
+  let newBook = Object.create(Book);
   newBook.title = document.getElementById("title").value;
   newBook.author = document.getElementById("author").value;
   newBook.pages = document.getElementById("pages").value;
 
   myLibrary.push(newBook);
   document.querySelector("#form").reset();
+  displayBook(newBook);
+}
+
+function displayBook(newBook) {
+  const newCard = document.createElement("div");
+  const cardTitle = document.createElement("p");
+  const cardAuthor = document.createElement("p");
+
+  newCard.classList.add("card");
+  cardTitle.classList.add("title");
+  cardAuthor.classList.add("author");
+
+  newCard.appendChild(cardTitle);
+  newCard.appendChild(cardAuthor);
+  cardContainer.appendChild(newCard);
+
+  cardTitle.innerText = newBook.title;
+  cardAuthor.innerText = newBook.author;
 }
 
 // Helper Functions
