7d5e262d33e3a5e20c231c59400df7fa614f6df0
Jordanjhiggins <jordan.higgins14@gmail.com>
2022-12-14 05:47:00 -0400

    add ability to remove book from array

--- script.js
+++ script.js
@@ -8,44 +8,53 @@ const cardContainer = document.getElementById("card-container");
 // Main App
 let myLibrary = [];
 
-function Book(title, author, pages) {
+// Book constructor.
+function Book(title, author, pages, id) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.id = id;
 }
 
+// Get values and add new book to library.
 function addBookToLibrary() {
-  let newBook = Object.create(Book);
+  let newBook = new Book();
+
   newBook.title = document.getElementById("title").value;
   newBook.author = document.getElementById("author").value;
   newBook.pages = document.getElementById("pages").value;
+  newBook.id = this.title.value;
 
   myLibrary.push(newBook);
   document.querySelector("#form").reset();
+
   displayBook(newBook);
 }
 
+// Create book
 function displayBook(newBook) {
   const newCard = document.createElement("div");
   const cardTitle = document.createElement("p");
   const cardAuthor = document.createElement("p");
-  const removeButton = document.createElement("button");
 
   newCard.classList.add("card");
   cardTitle.classList.add("title");
   cardAuthor.classList.add("author");
-  removeButton.classList.add("remove-button");
-
-  removeButton.setAttribute("onclick", "this.parentNode.remove()");
-  removeButton.textContent = "Remove";
 
   newCard.appendChild(cardTitle);
   newCard.appendChild(cardAuthor);
-  newCard.appendChild(removeButton);
   cardContainer.appendChild(newCard);
 
+  const removeButton = document.createElement("button");
+  removeButton.setAttribute("onclick", "this.parentNode.remove()");
+
+  newCard.appendChild(removeButton);
   cardTitle.innerText = newBook.title;
   cardAuthor.innerText = newBook.author;
+
+  removeButton.addEventListener("click", () => {
+    myLibrary.splice(myLibrary.indexOf(newBook), 1);
+  });
 }
 
 // Helper Functions
