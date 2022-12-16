1d456a8c9472b5d0073a510fb17333d98d599821
Jordanjhiggins <jordan.higgins14@gmail.com>
2022-12-16 15:52:05 -0400

    add ability to toggle if book has been read or not

--- script.js
+++ script.js
@@ -2,18 +2,17 @@ const addBookButton = document.getElementById("add-book");
 const openModalButtons = document.querySelectorAll("[data-modal-target]");
 const closeModalButtons = document.querySelectorAll("[data-close-button]");
 const overlay = document.getElementById("overlay");
-
 const cardContainer = document.getElementById("card-container");
 
 // Main App
 let myLibrary = [];
 
 // Book constructor.
-function Book(title, author, pages, id) {
+function Book(title, author, pages, readStatus) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.id = id;
+  this.readStatus = readStatus;
 }
 
 // Get values and add new book to library.
@@ -23,37 +22,60 @@ function addBookToLibrary() {
   newBook.title = document.getElementById("title").value;
   newBook.author = document.getElementById("author").value;
   newBook.pages = document.getElementById("pages").value;
-  newBook.id = this.title.value;
-
+  newBook.readStatus = 1;
   myLibrary.push(newBook);
-  document.querySelector("#form").reset();
 
-  displayBook(newBook);
+  formReset();
+  createBook(newBook);
 }
 
+Book.prototype.readToggle = (statusValue, button) => {
+  if (statusValue % 2 == 0) {
+    button.innerText = "Read";
+    button.classList.remove("toggle-inactive");
+    button.classList.add("toggle-active");
+  } else {
+    button.innerText = "Not read";
+    button.classList.remove("toggle-active");
+    button.classList.add("toggle-inactive");
+  }
+};
+
 // Create book
-function displayBook(newBook) {
+function createBook(newBook) {
   const newCard = document.createElement("div");
   const cardTitle = document.createElement("p");
   const cardAuthor = document.createElement("p");
+  let statusButton = document.createElement("button");
+  const removeButton = document.createElement("button");
 
+  // let thisReadStatus = readToggle(statusButton);
   newCard.classList.add("card");
   cardTitle.classList.add("title");
   cardAuthor.classList.add("author");
+  statusButton.classList.add("status");
+  // statusButton.classList.add("toggle-active");
+  // statusButton.classList.add("toggle-inactive");
 
   newCard.appendChild(cardTitle);
   newCard.appendChild(cardAuthor);
+  newCard.appendChild(statusButton);
+  newCard.appendChild(removeButton);
   cardContainer.appendChild(newCard);
 
-  const removeButton = document.createElement("button");
   removeButton.setAttribute("onclick", "this.parentNode.remove()");
 
-  newCard.appendChild(removeButton);
+  removeButton.innerText = "Remove";
   cardTitle.innerText = newBook.title;
   cardAuthor.innerText = newBook.author;
 
   removeButton.addEventListener("click", () => {
     myLibrary.splice(myLibrary.indexOf(newBook), 1);
+  });
+
+  statusButton.addEventListener("click", () => {
+    newBook.readStatus += 1;
+    newBook.readToggle(newBook.readStatus, statusButton);
   });
 }
 
@@ -62,8 +84,11 @@ function displayBook(newBook) {
 function handleForm(event) {
   event.preventDefault();
 }
-
 form.addEventListener("submit", handleForm);
+
+function formReset() {
+  document.querySelector("#form").reset();
+}
 
 // Modal
 openModalButtons.forEach((button) => {
